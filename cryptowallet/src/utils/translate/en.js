3424c518036d3b1a8775dbe293be259fd7257ef3
totaki <karagabul@gmail.com>
2018-03-03 17:37:02 +0300

    Add check generate mnemonics

--- cryptowallet/src/utils/translate/en.js
+++ cryptowallet/src/utils/translate/en.js
@@ -1,6 +1,7 @@
 const translates = {
     "INVALID_UUID": "This identification token not exists or was expired",
-    "SORRY": "Sorry, something wrong, try again later"
+    "SORRY": "Sorry, something wrong, try again later",
+    "CRYPTO_FALSE": "Your browser cannot using secure crypto functions please use last of Chrome, Firefox or Safari"
 };
 
 export default translates;
