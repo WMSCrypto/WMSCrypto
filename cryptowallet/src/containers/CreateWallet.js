61297b86eadafb200b6285a38306180fdbe2ab01
totaki <karagabul@gmail.com>
2018-05-24 18:50:54 +0300

    Make create wallet stateless

--- cryptowallet/src/containers/CreateWallet.js
+++ cryptowallet/src/containers/CreateWallet.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { AccountsGenerator } from '../components';
 import CreatePassword from "../components/CreatePassword";
-import { sendPut } from '../utils';
 import MnemonicsList from "../components/mnemonics/MnemonicsGenerator";
 import define from "../core/define";
 import CreateImage from "../components/CreateImage";
@@ -11,46 +10,22 @@ import SaveWallets from "../components/results/SaveWallets"
 import StepCounter from "../components/steps/StepCounter";
 
 
-class CreateWallet extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            accounts: null,
-            seed: null,
-            generated: false
-        };
-        this._onSave = this._onSave.bind(this);
-    }
-
-    _onSave() {
-        const { accounts } = this.state;
-        const { onOperationResult, uuid } = this.props;
-        sendPut(
-            uuid,
-            {accounts: accounts.map(e => [e.coin.id, e.node.neutered().toBase58()])},
-            onOperationResult)
-    }
-
-    render() {
-        const { uuid } = this.props.common;
-        return (
-            <StepCounter>
-                <CreatePassword first={true} next={define.steps.generateMnemonics}/>
-                <MnemonicsList next={define.steps.askMnemonic}/>
-                <MnemonicCheck next={define.steps.generateImage}/>
-                <CreateImage next={!!uuid && define.steps.generateXpub}/>
-                <AccountsGenerator next={define.steps.saveWallets} onlyOnline={true}/>
-                <SaveWallets onlyOnline={true} controls={false} last={true}/>
-            </StepCounter>
-        )
-    }
-}
+const CreateWallet  = ({ common }) => {
+    const { uuid } = common;
+    return (
+        <StepCounter>
+            <CreatePassword first={true} next={define.steps.generateMnemonics}/>
+            <MnemonicsList next={define.steps.askMnemonic}/>
+            <MnemonicCheck next={define.steps.generateImage}/>
+            <CreateImage next={!!uuid && define.steps.generateXpub}/>
+            <AccountsGenerator next={define.steps.saveWallets} onlyOnline={true}/>
+            <SaveWallets onlyOnline={true} controls={false} last={true}/>
+        </StepCounter>
+    )
+};
 
 CreateWallet.propTypes = {
-    seed: PropTypes.string,
-    uuid: PropTypes.string,
-    onOperationResult: PropTypes.func
+    common: PropTypes.object,
 };
 
 export default CreateWallet;
